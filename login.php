dcd9c6263f6122de5bd70a83ddbaae77c117a80a
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2024-08-23 21:31:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,5 @@
 <?php 
-session_start(); 
-include('configall.php');
 
-if (isset($_POST['uname']) && isset($_POST['password'])) {
-
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
-
-	$uname = validate($_POST['uname']);
-	$pass = validate($_POST['password']);
 
     //echo $role;
 
