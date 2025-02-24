d17060cf5bb61754fae41a9cbe8d7c12bd1fdee2
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2025-02-24 16:21:52 +1100

    Update Tp.py

--- Tp.py
+++ Tp.py
@@ -39,14 +39,6 @@ from datetime import datetime
         wind_speed = data['wind']['speed']
 
     
-
-    async def get_and_save_weather(self, city):
-        try:
-            data = await self.fetch_weather(city)
-            weather_report = self.parse_weather_data(data)
-            print(f"Weather in {city}:\n{weather_report}")
-            await self.save_weather_data(city, data)
-        except Exception as e:
             print(f"Error fetching weather data for {city}: {e}")
 
 async def main():
