328ef05aead3d3b67e2f1a0c956526ed5d1e5746
Atharva Shanware <146636925+Atharvams94@users.noreply.github.com>
2025-04-02 11:46:38 +1100

    Update login.php

--- login.php
+++ login.php
@@ -13,22 +13,7 @@
         window.location='home.php';
         </script>";
 	}else{
-		$sql = "SELECT * FROM admin WHERE username='$uname' AND password='$pass'";
-
-		$result = mysqli_query($connection, $sql);
-
-		if (mysqli_num_rows($result) == 1) {
-			$row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $uname && $row['password'] === $pass) {
-            	$_SESSION['user_name'] = $row['username'];
-            	#$_SESSION['name'] = $row['name'];
-				$_SESSION['id'] = $row['id'];			
-            	header("Location: admin_home_page.php?user exists");
-		        exit();
-            }else{
-				echo "<script type='text/javascript'>alert('Incorrect username or password');
-				window.location='home.php';
-				</script>";
+	
 			}
 		}else{
 			echo "<script type='text/javascript'>alert('Incorrect username or password');
